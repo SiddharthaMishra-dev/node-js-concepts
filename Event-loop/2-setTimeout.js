186c4a2903f733df6b4b3022800f7a1a1b539d1e
sidddharthamishra <mailbox.siddharthamishra@gmail.com>
2022-04-10 13:42:12 +0530

    Add event-loop files

---
+++ Event-loop/2-setTimeout.js
@@ -0,0 +1,9 @@
+console.log("This is first Line");
+setTimeout(() => {
+  console.log("This is second line");
+}, 0);
+console.log("This is third line");
+//Expected
+//This is first Line
+// This is second line
+// This is third line
---
+++ Event-loop/3-setinterval.js
@@ -0,0 +1,4 @@
+setInterval(() => {
+  console.log("This is a line");
+}, 1000);
+console.log("This is the end of program");
